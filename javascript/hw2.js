46af2a0e12c028739ebd94b51f4601d564d29131
patrickmccall <patrick.mccall@gmail.com>
2016-01-28 16:45:55 -0800

    update

---
+++ javascript/hw2.js
@@ -0,0 +1,17 @@
+﻿
+function myfunction() {
+  var firstReplacement = document.getElementById("firstReplacement").value;
+  var secondReplacement = document.getElementById("secondReplacement").value;
+  var thirdReplacement = document.getElementById("thirdReplacement").value;
+  var fourthReplacement = document.getElementById("fourthReplacement").value;
+  var fifthReplacement = document.getElementById("fifthReplacement").value;
+  var sixthReplacement = document.getElementById("sixthReplacement").value;
+
+
+//var originalQuote =   '"There is nothing wrong with America that cannot be cured by what is right with America." Bill Clinton'
+var editedQuote ="";
+editedQuote = '"There is ' + firstReplacement + ' ' + secondReplacement + ' with ' + thirdReplacement + ' that cannot be ' + fourthReplacement + ' by what is ' + fifthReplacement + ' with ' + sixthReplacement + '." Bill Clinton';
+console.log(editedQuote);
+//alert(editedQuote);
+}
+
